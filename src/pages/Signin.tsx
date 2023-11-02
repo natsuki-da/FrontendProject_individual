4340c54354fe9b13975b78b12739a7ed93f4ae75
natsuki.i <natsuki.iida0511@gmail.com>
2023-11-02 17:22:36 +0100

    changed some icons

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,19 +1,16 @@
-import { Button, Container } from "react-bootstrap"
-import "../app.css"
-import Dog from "/images/dog.svg"
+import { Container } from "react-bootstrap"
+import Dog from "/icons/dog.svg"
 
 function Signin (){
     return (
         <Container>
             <div className="sign-log-in">
-                <img src={Dog} height="100px" alt="logo_dog" />
-                <div>Sign in</div>
-                <input type="email" placeholder="E-mail address" />
-                <input type="password" placeholder="Password" />
-                <Button variant="primary">Sign in</Button>
-                
-                <div>Not a member?</div>
-                
+                <div><img src={Dog} height="100px" alt="logo_dog" /></div>
+                <div className="title">Sign in</div>
+                <input className="input" type="email" placeholder="E-mail address" />
+                <input className="input" type="password" placeholder="Password" />
+                <button className="button">Sign in</button>
+                <div className="message  pe-auto">Not a member? <a href="#">Sign up</a></div>
             </div>
         </Container>
     )
