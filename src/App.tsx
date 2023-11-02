89fe22fb88401a5067a2c2f2909e133c9e1de9a4
natsuki.i <natsuki.iida0511@gmail.com>
2023-11-02 17:21:13 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
-function App (){
-  return <h1>Hi</h1>
-}
+// import { Container } from "react-bootstrap"
+import { Navi } from "./components/Navi"
+import { Detail } from "./pages/Detail"
+import "./app.css"
 
-export default App
+// import { Route, Routes } from "react-router-dom"
+
+export function App (){
+  return (
+    <>
+    <Navi />
+    <Detail />
+    
+    {/* <Container>
+      <Routes>
+        <div>Hello</div>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Container> */}
+    </>
+
+    
+
+  )
+}
