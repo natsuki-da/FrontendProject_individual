310024824d90d1371c2d0626d80c4a5173167232
natsuki.i <natsuki.iida0511@gmail.com>
2023-11-08 15:22:59 +0100

    added a DateRange component

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,11 +1,40 @@
-import { Col, Row } from "react-bootstrap";
-
+import { Container} from "react-bootstrap";
+import Pin from "/icons/pin.svg";
+import Calender from "/icons/calender.svg";
+import  Human from "/icons/human.svg";
+import  Magnifying from "/icons/magnifying.svg";
+import { DateRange } from "react-date-range";
+import {useState} from 'react'
 
 export function Searchbar(){
+    const [date, setDate] = useState([
+        {
+          startDate: new Date(),
+          endDate: null,
+          key: 'selection'
+        }
+      ]);
     return (
-        <Row>
-            <Col><div></div>
-            </Col>
-        </Row>
+        <Container>
+            <div className="searchbar">
+                <div className="search-field">
+                    <img className="search-icons" src={Pin} alt=""/>
+                    <input className="search-inputs" type="search" placeholder="Enter destination" />
+                </div>
+                <div className="search-field">
+                    <img className="search-icons" src={Calender} alt=""/>
+                    <span className="search-inputs">Choose dates</span>
+                    {/* <input className="search-inputs" type="date" placeholder="Choose dates" /> */}
+                    <DateRange className="search-calender" editableDateInputs={true} onChange={item => setState([item.selection])} moveRangeOnFirstSelection={false} ranges={date} />
+                </div>
+                <div className="search-field">
+                    <img className="search-icons" src={Human} alt=""/>
+                    <input className="search-inputs" type="number" placeholder="Travelers"/>
+                </div>
+                    <button className="search-button">
+                        <img src={Magnifying} />
+                    </button>
+            </div>
+        </Container>
     )
 }
