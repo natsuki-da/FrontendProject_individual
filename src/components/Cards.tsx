a823e15f54de1988d2eb9430a187b0f7c1c08e8c
natsuki.i <natsuki.iida0511@gmail.com>
2023-11-08 15:23:37 +0100

    added some style

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,16 +1,36 @@
-import { CardImg, CardSubtitle, CardText, CardTitle, Col, Row } from "react-bootstrap";
+// import { CardImg, CardSubtitle, CardText, CardTitle, Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 export function Cards (){
     return (
-        <Row>
-            <Col lg={6}>
-              <CardImg></CardImg>
+      <Container>
+        <Row className="card">
+          <Col lg={6}>
+            <img className="card-image" />
+          </Col>
+          <Col lg={6}>
+            <h4>
+              Name
+            </h4>
+            <div className="price fs-5">
+              Price
+            </div>
+            <div className="conditions">
+            </div>
+            <div className="tag">
+              
+            </div>
+          </Col>
+
+            {/* <Col lg={6}>
+              <CardImg src={House1}></CardImg>
             </Col>
             <Col lg={6}>
               <CardTitle>Hi</CardTitle>
               <CardSubtitle>How Are You?</CardSubtitle>
               <CardText></CardText>
-            </Col>
+            </Col> */}
         </Row>
+      </Container>
     )
 }
