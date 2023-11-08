bd0c5b525bbe909c71713ccddd483e203f79143d
natsuki.i <natsuki.iida0511@gmail.com>
2023-11-08 15:23:13 +0100

    pending...

--- src/components/Navi.tsx
+++ src/components/Navi.tsx
@@ -1,6 +1,7 @@
 import { Container, Navbar } from "react-bootstrap"
 import Logo from "/icons/petstay.svg"
 import Human from "/icons/human.svg"
+// import { NavLink } from "react-router-dom"
 
 export function Navi (){
     return (
@@ -8,9 +9,11 @@ export function Navi (){
             <Container> 
                 <div><img src={Logo} height="50px" alt="logo"/></div>
                 <div className="pe-auto">
-                    <img src={Human} /> 
+                    <img src={Human} />
+                    {/* <Nav.Link as={NavLink} to={"/signin"}>Sign in</Nav.Link>
+                    <Nav.Link as={NavLink} to={"/signup"}>Sign up</Nav.Link>
                     <a href="#">Sign in</a>
-                    <a href="#"> / Sign up</a>
+                    <a href="#"> / Sign up</a> */}
                 </div>
             </Container>
         </Navbar>
