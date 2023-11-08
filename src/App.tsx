835b0f3bda37d5578f7e465a39f3f85e7b922b4c
natsuki.i <natsuki.iida0511@gmail.com>
2023-11-08 15:24:00 +0100

    added a BrowserRouter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-// import { Container } from "react-bootstrap"
-import { Navi } from "./components/Navi"
-import { Detail } from "./pages/Detail"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "./app.css"
-
-// import { Route, Routes } from "react-router-dom"
+import { Navi } from "./components/Navi"
+// import { Searchbar } from "./components/Searchbar"
+import { Home } from "./pages/Home"
+import { Detail } from "./pages/Detail"
+import { Signin } from "./pages/Signin"
+import { Signup } from "./pages/Signup"
 
 export function App (){
   return (
     <>
     <Navi />
-    <Detail />
-    
-    {/* <Container>
-      <Routes>
-        <div>Hello</div>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Container> */}
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/detail" element={<Detail />}></Route>
+      <Route path="/signin" element={<Signin />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+     </Routes>
+    </BrowserRouter>
     </>
-
-    
 
   )
 }
