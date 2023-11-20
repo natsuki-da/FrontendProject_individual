2ef70c4c3c926dc33a4f6b13d2ea594ddb329f4d
natsuki.i <natsuki.iida0511@gmail.com>
2023-11-20 17:19:48 +0100

    testing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,21 +1,53 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { Cards } from "../components/Cards";
-import { Searchbar } from "../components/Searchbar";
+// import { Container, Row } from "react-bootstrap";
+
+// import { Searchbar } from "../components/Searchbar";
+// import { useFetch } from "../hooks/useFetch";
 
 
 export function Home(){
+    // const {data, loading } = useFetch("http://localhost:5000/api/property/get")
+    // console.log(data)
     return (
-        <Container>
-            <Searchbar />
-            {/* <Row>
+         <Container>
+            {/* { loading ? ("Loading...please wait") : 
+             (
+                data.map((property:any) => (
+                    <>
+                    <h2>{property.name}</h2>
+                    <div>{property.type}</div>
+                    <div>{property.city}</div>
+                    </>
+                )
+                )
+             )} */}
+             
+             <Row> 
                 <Col xs={12} md={12} lg={6}>
                     <Cards />
                 </Col>
                 <Col xs={12} md={12} lg={6}>
                     <Cards />
                 </Col>
-            </Row> */}
+            </Row>
         </Container>
-        
+        //     {/* <Searchbar /> */}
+        //     { loading ? ("Loading...please wait") : 
+        //     (
+        //     <>
+        //     <div>{data[0].name}</div>
+                
+        //     <Row>
+                
+        //         {/* <Col xs={12} md={12} lg={6}>
+        //             <Cards />
+        //         </Col>
+        //         <Col xs={12} md={12} lg={6}>
+        //             <Cards />
+        //         </Col> */}
+        //     </Row> </>
+        //     )}
+        // </Container>
     )
 }
