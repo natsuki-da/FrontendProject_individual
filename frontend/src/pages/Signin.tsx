68039898b7886ba80ba54fab503ed79d1f9c38e5
natsuki.i <natsuki.iida0511@gmail.com>
2023-11-20 17:19:23 +0100

    🚧

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,15 +1,35 @@
 import { Container } from "react-bootstrap"
 import Dog from "/icons/dog.svg"
+import axios from "axios"
+import { useState } from "react";
 
 export function Signin (){
+    const [userEmail, setEmail] = useState("");
+    const [userPassword, setPassword] = useState("");
+    
+    const login = () => {
+        const r = axios.get("http://localhost:5000/api/auth/signin",
+            {
+                params: {
+                    email: userEmail,
+                    password: userPassword
+                }
+            })
+        
+    };
+
     return (
+
         <Container>
+            <div>{r.data}</div>
             <div className="sign-log-in">
                 <div><img src={Dog} height="100px" alt="logo_dog" /></div>
                 <div className="title">Sign in</div>
-                <input className="input" type="email" placeholder="E-mail address" />
-                <input className="input" type="password" placeholder="Password" />
-                <button className="button">Sign in</button>
+                <form onSubmit={login}>
+                    <input className="input" type="email" placeholder="E-mail address" onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {setEmail(e.target.value);}}/>
+                    <input className="input" type="password" placeholder="Password" onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {setPassword(e.target.value);}}/>
+                    <button className="button">Sign in</button>
+                </form>
                 <div className="message  pe-auto">Not a member? <a href="#">Sign up</a></div>
             </div>
         </Container>
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,15 +1,35 @@
 import { Container } from "react-bootstrap"
 import Dog from "/icons/dog.svg"
+import axios from "axios";
+import { useState } from "react";
 
 export function Signup (){
+    const [userName, setUsername] = useState("");
+    const [userEmail, setEmail] = useState("");
+    const [userPassword, setPassword] = useState("");
+
+    //Register user via API
+    const register = () => {
+        // console.log("hej")
+        axios.post("http://localhost:5000/api/auth/signup", {
+            username: userName, 
+            email: userEmail, 
+            password: userPassword,
+        })
+
+    };
+
     return (
         <Container>
             <div className="sign-log-in">
                 <div><img src={Dog} height="100px" alt="logo_dog" /></div>
                 <div className="title">Sign up</div>
-                <input className="input" type="email" placeholder="E-mail address" />
-                <input className="input" type="password" placeholder="Password" />
-                <button className="button">Sign up</button>
+                <form onSubmit={register}>
+                    <input className="input" type="username" placeholder="User name" onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {setUsername(e.target.value);}} required/>
+                    <input className="input" type="email" placeholder="E-mail address" onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>{setEmail(e.target.value);}} required/>
+                    <input className="input" type="password" placeholder="Password" onChange={(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>{setPassword(e.target.value);}} required/>
+                    <button className="button">Sign up</button>
+                </form>
                 <div className="message  pe-auto">Already a member? <a href="#">Sign in</a></div>
             </div>
         </Container>
