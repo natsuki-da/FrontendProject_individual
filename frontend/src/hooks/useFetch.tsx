3b978f1e23751f095e0791ad5591cb7744952179
natsuki.i <natsuki.iida0511@gmail.com>
2023-11-20 17:19:06 +0100

    created a fetch-data-from-db function

---
+++ frontend/src/hooks/useFetch.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+export function useFetch (url:any) {
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const res = await axios.get(url);
+                setData(res.data);
+            } catch(err:any){
+                setError(err);
+            }
+            setLoading(false)
+        };
+        fetchData();
+    }, [url]);
+    
+    // const reFetch = async () => {
+    //     setLoading(true);
+    // try {
+    //     const res = await axios.get(url);
+    //     setData(res.data);
+    // } catch(err:any){
+    //     setError(err);
+    // }
+    // setLoading(false)
+    // };
+    return { data, loading, error, 
+ }
+};
