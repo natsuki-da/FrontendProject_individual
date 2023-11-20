992e0d1b0d2c5fb351039d194a02ca780a049c8b
natsuki.i <natsuki.iida0511@gmail.com>
2023-11-20 17:20:24 +0100

    implemented a useFetch function

--- frontend/src/components/Cards.tsx
+++ frontend/src/components/Cards.tsx
@@ -1,36 +1,53 @@
 // import { CardImg, CardSubtitle, CardText, CardTitle, Col, Container, Row } from "react-bootstrap";
 import { Col, Container, Row } from "react-bootstrap";
 
-export function Cards (){
-    return (
-      <Container>
-        <Row className="card">
-          <Col lg={6}>
-            <img className="card-image" />
-          </Col>
-          <Col lg={6}>
-            <h4>
-              Name
-            </h4>
-            <div className="price fs-5">
-              Price
-            </div>
-            <div className="conditions">
-            </div>
-            <div className="tag">
-              
-            </div>
-          </Col>
+import { useFetch } from "../hooks/useFetch";
 
-            {/* <Col lg={6}>
-              <CardImg src={House1}></CardImg>
-            </Col>
-            <Col lg={6}>
-              <CardTitle>Hi</CardTitle>
-              <CardSubtitle>How Are You?</CardSubtitle>
-              <CardText></CardText>
-            </Col> */}
-        </Row>
-      </Container>
-    )
+export function Cards (){
+  const {data, loading } = useFetch("http://localhost:5000/api/property/get")
+    console.log(data)
+  return (
+    <Container>
+      { loading ? ("Loading...please wait") : 
+             (
+                data.map((property:any) => (
+                  <Row key={property.id} className="card">
+                    <Col lg={6}></Col>
+                    <Col lg={6}>
+                      <h4>{property.name}</h4>
+                      <div className="price fs-5">{property.price}</div>
+                      <div className="conditions"></div>
+                      <div className="tag">{property.type}</div>
+                    </Col>
+                  </Row>
+                )
+                )
+             )}
+    </Container>
+  )
+
+
+
+    // return (
+    //   <Container>
+    //     <Row className="card">
+    //       <Col lg={6}>
+    //         <img className="card-image" />
+    //       </Col>
+    //       <Col lg={6}>
+    //         <h4>
+    //           Name
+    //         </h4>
+    //         <div className="price fs-5">
+    //           Price
+    //         </div>
+    //         <div className="conditions">
+    //         </div>
+    //         <div className="tag">
+              
+    //         </div>
+    //       </Col>
+    //     </Row>
+    //   </Container>
+    // )
 }
