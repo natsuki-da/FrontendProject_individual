7ef534ca2cfebf1f239934c0039ab6d7de9e73bb
natsuki.i <natsuki.iida0511@gmail.com>
2023-11-27 00:08:58 +0100

    final

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,10 @@ import { Home } from "./pages/Home"
 import { Detail } from "./pages/Detail"
 import { Signin } from "./pages/Signin"
 import { Signup } from "./pages/Signup"
+import { LoginSuccess } from "./pages/LoginSuccess"
+import { CreateSuccess } from "./pages/CreateSuccess"
+import { Confirm } from "./pages/Confirm"
+import { MessageSent } from "./pages/MessageSent"
 
 export function App (){
   return (
@@ -17,6 +21,11 @@ export function App (){
       <Route path="/detail" element={<Detail />}></Route>
       <Route path="/signin" element={<Signin />}></Route>
       <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/login-success" element={<LoginSuccess />}></Route>
+      <Route path="/login-success" element={<LoginSuccess />}></Route>
+      <Route path="/create-success" element={<CreateSuccess />}></Route>
+      <Route path="/confirm" element={<Confirm />}></Route>
+      <Route path="/sent" element={<MessageSent />}></Route>
      </Routes>
     </BrowserRouter>
     </>
